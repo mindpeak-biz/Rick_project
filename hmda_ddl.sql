c9ea2cf0be330d07ea5fb148abf22d6090dfcb0a
Aki Iskandar <aki@mindpeak.biz>
2021-06-29 08:35:32 -0600

    ai/added the new hmda table schema

--- hmda_ddl.sql
+++ hmda_ddl.sql
@@ -1,4 +1,13 @@
-CREATE TABLE hmda (
+-- here are the columns that we want to store:
+-- 0 activity_year
+-- 1 lei
+-- 8 derived_dwelling_category
+-- 15 loan_type
+-- 21 loan_amount
+-- 40 occupancy_type
+
+
+CREATE TABLE old_hmda (
 	id serial,
     as_of_year char(4) NULL,
     respondent_id varchar(20) NULL,
@@ -10,4 +19,13 @@ CREATE TABLE hmda (
 );
 
 
-                                                      
+CREATE TABLE new_hmda (
+	id serial,
+    activity_year char(4) NULL,
+    lei varchar(32) NULL,
+    derived_dwelling_category varchar(256) NULL,
+    loan_type int2 NULL,
+    loan_amount numeric(8,2) NOT NULL,
+    occupancy_type int2 NULL,
+	CONSTRAINT new_hmda_pkey1 PRIMARY KEY (id)
+);                                                      
--- split_large_file.py
+++ split_large_file.py
@@ -26,7 +26,7 @@ large_file_name      = '/Users/aki/dev/big_data_files/FullDataFor2019.csv'
 year_for_data        = 2019
 output_directory     = f'/Users/aki/dev/big_data_files/HMDA_{year_for_data}'
 max_lines_per_file   = 100000.0
-desired_cols         = [0, 1, 3, 21, 45, 46, 23]
+desired_cols         = [0, 1, 8, 15, 21, 40]
 
 
 # Program variables
@@ -86,6 +86,7 @@ def chop_it_up():
         for x in desired_cols:
             desired_column_row_list.append(current_line_columns[x])
 
+        # close off the previous file and start a new file every x many records
         if current_line_number % max_lines_per_file == 0.0:
             close_current_file()
             time.sleep(2) 
@@ -100,7 +101,7 @@ def chop_it_up():
 
 def process_line(current_line_number, desired_column_row_list):
     global f 
-    list_line_to_write = [current_line_number, desired_column_row_list[0], desired_column_row_list[1], desired_column_row_list[2], desired_column_row_list[3], desired_column_row_list[4], desired_column_row_list[5], desired_column_row_list[6]] 
+    list_line_to_write = [current_line_number, desired_column_row_list[0], desired_column_row_list[1], desired_column_row_list[2], desired_column_row_list[3], desired_column_row_list[4], desired_column_row_list[5]] 
     list_line_to_write = [str(x) for x in list_line_to_write]
     # write out the current line's desired columns
     line_to_write = ",".join(list_line_to_write)
@@ -114,7 +115,7 @@ def start_new_file():
     current_file_name = f'{output_directory}/hmda_{current_file_number}.csv'
     f = open(current_file_name,"a+")
     # write out the header row
-    desired_header_line = ",".join(['record_number', desired_column_headers_list[0], desired_column_headers_list[1], desired_column_headers_list[2], desired_column_headers_list[3], desired_column_headers_list[4], desired_column_headers_list[5], desired_column_headers_list[6]]) 
+    desired_header_line = ",".join(['record_number', desired_column_headers_list[0], desired_column_headers_list[1], desired_column_headers_list[2], desired_column_headers_list[3], desired_column_headers_list[4], desired_column_headers_list[5]]) 
     f.write(desired_header_line)
     f.write('\n')
     print(current_file_name)
@@ -130,3 +131,105 @@ if __name__ == "__main__":
     main()
 
 
+'''
+These are the available columns in the 2019 HMDA csv file:
+* 0 activity_year
+* 1 lei
+2 derived_msa-md
+3 state_code
+4 county_code
+5 census_tract
+6 conforming_loan_limit
+7 derived_loan_product_type
+* 8 derived_dwelling_category
+9 derived_ethnicity
+10 derived_race
+11 derived_sex
+12 action_taken
+13 purchaser_type
+14 preapproval
+* 15 loan_type
+16 loan_purpose
+17 lien_status
+18 reverse_mortgage
+19 open-end_line_of_credit
+20 business_or_commercial_purpose
+* 21 loan_amount
+22 loan_to_value_ratio
+23 interest_rate
+24 rate_spread
+25 hoepa_status
+26 total_loan_costs
+27 total_points_and_fees
+28 origination_charges
+29 discount_points
+30 lender_credits
+31 loan_term
+32 prepayment_penalty_term
+33 intro_rate_period
+34 negative_amortization
+35 interest_only_payment
+36 balloon_payment
+37 other_nonamortizing_features
+38 property_value
+39 construction_method
+* 40 occupancy_type
+41 manufactured_home_secured_property_type
+42 manufactured_home_land_property_interest
+43 total_units
+44 multifamily_affordable_units
+45 income
+46 debt_to_income_ratio
+47 applicant_credit_score_type
+48 co-applicant_credit_score_type
+49 applicant_ethnicity-1
+50 applicant_ethnicity-2
+51 applicant_ethnicity-3
+52 applicant_ethnicity-4
+53 applicant_ethnicity-5
+54 co-applicant_ethnicity-1
+55 co-applicant_ethnicity-2
+56 co-applicant_ethnicity-3
+57 co-applicant_ethnicity-4
+58 co-applicant_ethnicity-5
+59 applicant_ethnicity_observed
+60 co-applicant_ethnicity_observed
+61 applicant_race-1
+62 applicant_race-2
+63 applicant_race-3
+64 applicant_race-4
+65 applicant_race-5
+66 co-applicant_race-1
+67 co-applicant_race-2
+68 co-applicant_race-3
+69 co-applicant_race-4
+70 co-applicant_race-5
+71 applicant_race_observed
+72 co-applicant_race_observed
+73 applicant_sex
+74 co-applicant_sex
+75 applicant_sex_observed
+76 co-applicant_sex_observed
+77 applicant_age
+78 co-applicant_age
+79 applicant_age_above_62
+80 co-applicant_age_above_62
+81 submission_of_application
+82 initially_payable_to_institution
+83 aus-1
+84 aus-2
+85 aus-3
+86 aus-4
+87 aus-5
+88 denial_reason-1
+89 denial_reason-2
+90 denial_reason-3
+91 denial_reason-4
+92 tract_population
+93 tract_minority_population_percent
+94 ffiec_msa_md_median_family_income
+95 tract_to_msa_income_percentage
+96 tract_owner_occupied_units
+97 tract_one_to_four_family_homes
+98 tract_median_age_of_housing_units
+'''
