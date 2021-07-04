25dd51a552be5eee755f1038d5fee89d8f7e7ac8
Aki Iskandar <aki@mindpeak.biz>
2021-07-04 17:22:00 -0600

    interim commit

--- misc_queries.sql
+++ misc_queries.sql
@@ -28,3 +28,23 @@ from
 from hmda 
 group by respondent_id) foo 
 order by foo.amt_of_mtgs desc; 
+
+
+
+-- July 4th
+
+
+select ts.activity_year, ts.agency_code, ac.description, 
+ts.respondent_id, ts.respondent_name, sum(h.loan_amount_in_000s)  
+from public.transmittal_sheet ts 
+
+join hmda h on h.lender_id = ts.respondent_id  
+join agency_code ac on ts.agency_code::int = ac.code  
+join events e on e.acq_instname = ts.respondent_name 
+
+--where ts.respondent_name like '%PNC%' 
+and ts.activity_year = '2019'
+
+group by ts.activity_year, ts.respondent_name, ts.agency_code, 
+      ac.description, ts.respondent_id     
+order by ts.activity_year, ts.respondent_name;
